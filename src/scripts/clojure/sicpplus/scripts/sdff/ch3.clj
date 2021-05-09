9f39ff33cd187c482701d4360babd85636cd8045
palisades-lakes <palisades.lakes@gmail.com>
2021-05-09 16:47:32 -0700

    start sdff ch 3

---
+++ src/scripts/clojure/sicpplus/scripts/sdff/ch3.clj
@@ -0,0 +1,24 @@
+;; core src/scripts/clojure/sicpplus/scripts/sdff/ch232.core
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+;;----------------------------------------------------------------
+(ns sicpplus.scripts.sdff.ch3
+  
+  {:doc "examples from SDFF chapter 3"
+   :author "palisades dot lakes at gmail dot com"
+   :version "2021-05-09"}
+  
+  (:refer-clojure :exclude [*])
+  (:require [clojure.core :as core]
+            [clojure.java.io :as io]
+            [clojure.string :as s]
+            [sicpplus.commons.core :as scc]
+            [sicpplus.cartesian :as cartesian])
+  (:import 
+    [java.lang Math]
+    [clojure.lang IFn IFn$DD Keyword]))
+
+;;----------------------------------------------------------------
+
+(scc/echo
+  (glsr-US 14.7 68 1))
